0e43b810b0652c6e526b0bc00148ae8081492be7
DhanaTontanahal <urwithdhanu@gmail.com>
2024-01-28 10:39:14 +0530

    webpack usage

---
+++ src/generateToken.js
@@ -0,0 +1,5 @@
+function generateToken() {
+  return "KDJYUGFIUDKVJ, FDUYKJ";
+}
+
+export default generateToken;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import generateToken from "./generateToken";
+import "./styles/main.scss";
+import laughing from "./assets/laughing-emoji.svg";
+
+const laughImg = document.getElementById("laughImg");
+laughImg.src = laughing;
+
+console.log(generateToken());
+console.log(1);
+console.log(2);
